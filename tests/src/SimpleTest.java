127a33588ce7ee06283f04cac2fbdf0175267a74
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-10-14 21:09:06 +0400

    test

--- tests/src/SimpleTest.java
+++ tests/src/SimpleTest.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 public class SimpleTest extends BaseTestCase {
   @Test
   public void testSendMessage() {
-    final TeamRoomMessage message = VSOTeamRoomsAPI.createConnection("ekoshkin", "R42apfoo").sendMessageToRoom("ekoshkin", 11141L, "hello!");
+    final TeamRoomMessage message = VSOTeamRoomsAPI.createConnection("aaa", "aaa").sendMessageToRoom("aaa", 111L, "hello!");
     assertNotNull(message);
     assertEquals("hello!", message.getContent());
   }
 
   @Test
   public void testGetListOfRooms() {
-    assertNotEmpty(VSOTeamRoomsAPI.createConnection("ekoshkin", "R42apfoo").getListOfRooms("ekoshkin"));
+    assertNotEmpty(VSOTeamRoomsAPI.createConnection("aaa", "aaa").getListOfRooms("aaa"));
   }
 }
