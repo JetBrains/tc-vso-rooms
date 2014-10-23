47015e0f34dfdb73e663f21087b149b97db3bdc1
Evgeniy Koshkin <evgeniy.koshkin@jetbrains.com>
2014-10-23 18:23:58 +0400

    improved team room ids caching; tests added

--- server/src/jetbrains/buildServer/vsoRooms/notificator/VSOTeamRoomIdsCache.java
+++ server/src/jetbrains/buildServer/vsoRooms/notificator/VSOTeamRoomIdsCache.java
@@ -16,6 +16,7 @@
 
 package jetbrains.buildServer.vsoRooms.notificator;
 
+import com.intellij.openapi.util.Pair;
 import jetbrains.buildServer.vsoRooms.rest.TeamRoom;
 import jetbrains.buildServer.vsoRooms.rest.VSOTeamRoomsAPIConnection;
 import org.apache.log4j.Logger;
@@ -32,20 +33,22 @@ public class VSOTeamRoomIdsCache {
 
   private static final Logger LOG = Logger.getLogger(VSOTeamRoomIdsCache.class);
 
-  private final Map<String, Long> myNameToIdMap = new HashMap<String, Long>();
+  private final Map<Pair<String, String>, Long> myNameToIdMap = new HashMap<Pair<String, String>, Long>();
 
   @Nullable
   public Long getOrResolveRoomId(@NotNull String vsoAccount, @NotNull String teamRoomName, @NotNull VSOTeamRoomsAPIConnection apiConnection) {
-    final String roomNameToLowerCase = teamRoomName.toLowerCase();
-    if(!myNameToIdMap.containsKey(roomNameToLowerCase)){
+    final String accountToLower = vsoAccount.toLowerCase();
+    final String teamRoomNameToLower = teamRoomName.toLowerCase();
+    final Pair<String, String> roomUUID = new Pair<String, String>(accountToLower, teamRoomNameToLower);
+    if(!myNameToIdMap.containsKey(roomUUID)){
       synchronized (myNameToIdMap){
-        if(!myNameToIdMap.containsKey(roomNameToLowerCase)){
-          LOG.debug(String.format("Team room ID for room name %s is not resolved yet. Calling VSO API to list all Team Rooms for account %s", roomNameToLowerCase, vsoAccount));
+        if(!myNameToIdMap.containsKey(roomUUID)){
+          LOG.debug(String.format("Team room ID for room name %s is not resolved yet. Calling VSO API to list all Team Rooms for account %s", teamRoomName, vsoAccount));
           try {
             for (TeamRoom teamRoom : apiConnection.getListOfRooms(vsoAccount)) {
-              final String resolvedRoomName = teamRoom.getName().toLowerCase();
-              if(!myNameToIdMap.containsKey(resolvedRoomName)){
-                myNameToIdMap.put(resolvedRoomName, teamRoom.getId());
+              final Pair<String, String> resolvedRoomUUID = new Pair<String, String>(accountToLower, teamRoom.getName().toLowerCase());
+              if(!myNameToIdMap.containsKey(resolvedRoomUUID)){
+                myNameToIdMap.put(resolvedRoomUUID, teamRoom.getId());
               }
             }
           } catch (Exception e) {
@@ -54,6 +57,6 @@ public class VSOTeamRoomIdsCache {
         }
       }
     }
-    return myNameToIdMap.get(roomNameToLowerCase);
+    return myNameToIdMap.get(roomUUID);
   }
 }
--- tests/src/VSOTeamRoomIdsCacheTest.java
+++ tests/src/VSOTeamRoomIdsCacheTest.java
@@ -14,25 +14,80 @@
  * limitations under the License.
  */
 
+import com.google.common.collect.Lists;
 import jetbrains.buildServer.BaseTestCase;
+import jetbrains.buildServer.vsoRooms.notificator.VSOTeamRoomIdsCache;
+import jetbrains.buildServer.vsoRooms.rest.TeamRoom;
+import jetbrains.buildServer.vsoRooms.rest.VSOTeamRoomsAPIConnection;
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 /**
  * @author Evgeniy.Koshkin
  */
 public class VSOTeamRoomIdsCacheTest extends BaseTestCase {
+
+  private VSOTeamRoomIdsCache myCache;
+  private Mockery myMockery;
+
+  @Override
+  @BeforeMethod
+  public void setUp() throws Exception {
+    super.setUp();
+    myCache = new VSOTeamRoomIdsCache();
+    myMockery = new Mockery();
+  }
+
   @Test
-  public void test_room_names_collision_in_two_accounts() throws Exception {
-    fail();
+  public void test_room_names_collision_in_separate_accounts() throws Exception {
+    final VSOTeamRoomsAPIConnection apiConnection = myMockery.mock(VSOTeamRoomsAPIConnection.class);
+    myMockery.checking(new Expectations() {{
+      one(apiConnection).getListOfRooms("accountA");
+      will(returnValue(Lists.newArrayList(new TeamRoom(1L, "room", "accountA room"))));
+      one(apiConnection).getListOfRooms("accountB");
+      will(returnValue(Lists.newArrayList(new TeamRoom(2L, "room", "accountB room"))));
+    }});
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("accountA", "room", apiConnection));
+    assertEquals(Long.valueOf(2), myCache.getOrResolveRoomId("accountB", "room", apiConnection));
+    myMockery.assertIsSatisfied();
   }
 
   @Test
   public void should_be_case_insensitive_for_team_room_name() throws Exception {
-    fail();
+    final VSOTeamRoomsAPIConnection apiConnection = myMockery.mock(VSOTeamRoomsAPIConnection.class);
+    myMockery.checking(new Expectations() {{
+      one(apiConnection).getListOfRooms("account");
+      will(returnValue(Lists.newArrayList(new TeamRoom(1L, "Room", "account room"))));
+    }});
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("account", "ROOM", apiConnection));
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("account", "room", apiConnection));
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("account", "Room", apiConnection));
+    myMockery.assertIsSatisfied();
   }
 
   @Test
   public void should_be_case_insensitive_for_account() throws Exception {
-    fail();
+    final VSOTeamRoomsAPIConnection apiConnection = myMockery.mock(VSOTeamRoomsAPIConnection.class);
+    myMockery.checking(new Expectations() {{
+      one(apiConnection).getListOfRooms("account");
+      will(returnValue(Lists.newArrayList(new TeamRoom(1L, "room", "account room"))));
+    }});
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("account", "room", apiConnection));
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("Account", "room", apiConnection));
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("ACCOUNT", "room", apiConnection));
+    myMockery.assertIsSatisfied();
+  }
+
+  @Test
+  public void should_resolve_all_account_room_ids_on_first_call() throws Exception {
+    final VSOTeamRoomsAPIConnection apiConnection = myMockery.mock(VSOTeamRoomsAPIConnection.class);
+    myMockery.checking(new Expectations() {{
+      one(apiConnection).getListOfRooms("account");
+      will(returnValue(Lists.newArrayList(new TeamRoom(1L, "room1", "account room 1"), new TeamRoom(2L, "room2", "account room 2"))));
+    }});
+    assertEquals(Long.valueOf(1), myCache.getOrResolveRoomId("account", "room1", apiConnection));
+    assertEquals(Long.valueOf(2), myCache.getOrResolveRoomId("account", "room2", apiConnection));
   }
 }
